85fcfbcb4626470ae4a15c3da0356213056f26cd
FranciscoXDPNeto <nintendousafran@gmail.com>
2026-06-11 16:51:02 -0300

    Create themes.js

---
+++ src/styles/themes.js
@@ -0,0 +1,17 @@
+export const temaClaro = {
+  fundo: "#f4f4f4",
+  texto: "#222",
+  card: "#ffffff",
+  primaria: "#4f46e5",
+  secundaria: "#e0e7ff",
+  perigo: "#dc2626",
+};
+
+export const temaEscuro = {
+  fundo: "#121212",
+  texto: "#f4f4f4",
+  card: "#1f1f1f",
+  primaria: "#818cf8",
+  secundaria: "#312e81",
+  perigo: "#f87171",
+};
